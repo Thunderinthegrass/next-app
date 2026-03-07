e3c6655495aecf6f49e2db14cd01f4b5034fa542
ThunderInTheGrass <propeller8924@gmail.com>
2026-03-07 18:17:08 +0800

    routing

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,14 @@ import Link from "next/link";
 export default function Home() {
   const [counter, setCounter] = useState(0);
   return (
-    <div>
+    <div className="flex justify-center h-full">
       <h1>Братск</h1>
-      <Link href='/about'>О нас</Link>
-      <Link href='/dashboard'>Панель управления</Link>
+      <div className="flex flex-col bg-green-300">
+        <Link href='/about'>О нас</Link>
+        <Link href='/dashboard'>Панель управления</Link>
+        <Link href='/products'>Продукты</Link>
+        <Link href='/posts'>Посты</Link>
+      </div>
       <button onClick={() => setCounter(prev => prev + 1)}>Прибавить</button>
       <button onClick={() => setCounter(prev => prev - 1)}>Убавить</button>
       {counter}
---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,16 @@
+const Post = async ({params} : {params: Promise<{postId: string}>}) => {
+
+  const { postId } = await params;
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const post = await response.json();
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+export default Post;
