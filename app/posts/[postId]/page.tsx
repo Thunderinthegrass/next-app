c6f038d07cef595217a370efbf44f1e9ea29840b
ThunderInTheGrass <propeller8924@gmail.com>
2026-03-08 21:35:07 +0800

    Разрешаем конфликты

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -10,7 +10,7 @@ const Post = async ({params} : {params: Promise<{postId: string}>}) => {
       <h1>{post.title} second</h1>
       <p>{post.body}</p>
       <p>{post.body}</p>
-      <span>Подсократил посты</span>
+      <span>подсократил посты</span>
     </div>
   );
 };
