37f38b797186fe81b00048306991d0a7cb363108
ThunderInTheGrass <propeller8924@gmail.com>
2026-03-11 17:42:46 +0800

    Вложенные динамические маршруты

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 async function getPosts() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   if (!res.ok) {
@@ -14,7 +21,7 @@ const Page = async () => {
   return (
     <div>
       <h2>Посты</h2>
-      {posts.map((post) => (
+      {posts.map((post: Post) => (
         <Link key={post.id} href={`/posts/${post.id}`}>{post.id}</Link>
       ))}
     </div>
